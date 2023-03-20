da9c411aae91b6857637d717e0331bf3d7383c4d
RngValue <104089072+RngValue@users.noreply.github.com>
2023-03-20 18:41:19 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const navbar = document.querySelector(".navbar");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting){
+            navbar.classList.add("nav-scrolled");
+        }else{
+            navbar.classList.remove("nav-scrolled");
+        }
+    });
+});
+
+
+const pageSectionStuff = document.querySelectorAll("#everything");
+pageSectionStuff.forEach((el) => observer.observe(el));
